0cb43a95cfa70a2012198205088ec72ee9046a6a
dev-sire <shahidamaan2019@gmail.com>
2025-09-17 23:19:16 +0500

    Feat: added Authentication

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,58 @@
+import { onSignInUser } from "@/actions/auth"
+import SignInForm from "@/components/forms/sign-in"
+import { GoogleAuthButton } from "@/components/global/google-0auth-button"
+import { Separator } from "@/components/ui/separator"
+import { currentUser } from "@clerk/nextjs/server"
+import Link from "next/link"
+import { redirect } from "next/navigation"
+
+type Props = {}
+
+const SignIn = async (props: Props) => {
+    const user = await currentUser()
+
+    if (user) {
+        // Only sign in the user if they exist
+        const authenticated = await onSignInUser(user.id)
+
+        if (authenticated.status === 200) {
+            redirect("/callback/sig-in")
+        } else if (authenticated.status === 400) {
+            redirect("/callback/sign-in") // Redirect to error page for failed sign-in
+        }
+
+        return (
+            <div className="flex flex-col gap-5">
+                <h5>You are already signed in.</h5>
+                <p>Redirecting...</p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h5 className="font-bold text-base text-themeTextWhite">Login</h5>
+            <p className="text-themeTextGray leading-tight">
+                Network with people from around the world, join groups, create
+                your own, watch courses and become the best version of yourself.
+            </p>
+            <SignInForm />
+            <div className="my-10 w-full relative">
+                <div className="bg-black p-3 absolute text-themeTextGray text-xs top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    OR CONTINUE WITH
+                </div>
+                <Separator orientation="horizontal" className="bg-themeGray" />
+            </div>
+            <GoogleAuthButton method="signin" />
+            <p className="mt-3 mb-1 text-center">
+                If you dont have an account, you can{" "}
+                <Link href={"/sign-up"} className="underline">
+                    SignUp
+                </Link>{" "}
+                here.
+            </p>
+        </div>
+    )
+}
+
+export default SignIn
--- src/app/(landing)/_components/pricing/index.tsx
+++ src/app/(landing)/_components/pricing/index.tsx
@@ -1,5 +1,9 @@
 import BackdropGradient from "@/components/global/backdrop-gradient"
 import GradientText from "@/components/global/gradient-text"
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardTitle } from "@/components/ui/card"
+import { Check } from "@/icons/check"
+import Link from "next/link"
 
 type Props = {}
 
@@ -9,17 +13,61 @@ export const PricingSection = (props: Props) => {
         className='w-full pt-20 flex flex-col items-center gap-3'
         id="pricing"
     >
-        <BackdropGradient className="w-8/12 h-full flex flex-col opacity-40 items-center">
+        <BackdropGradient className="w-8/12 h-full opacity-40 flex flex-col items-center">
             <GradientText
                 className="text-4xl font-semibold text-center"
-                element="H2"
+                element="H1"
             >
-                Pricng Plans That Fit You Right
+                Pricing Plans That Fit Your Right
             </GradientText>
-            <p className="text-sm md:text-center text-left text-muted-foreground">
-                Ascendia is a vibrant online community that empowers people to connect, <br className='md:hidden' />{" "} collaborate, and cultivate meaningful <br className='md:hidden' /> relationships.
+            <p className="text-sm md:text-center text-center mt-3 text-muted-foreground">
+                Ascendia is a vibrant online community platform that empowers
+                people to connect, <br className="hidden md:block" />
+                collaborate, and cultivate meaningful relationships
             </p>
         </BackdropGradient>
+        <Card className="p-7 mt-10 md:w-auto w-full bg-themeBlack border-themeGray">
+            <div className="flex flex-col gap-2">
+                <CardTitle className="">999/m</CardTitle>
+                <CardDescription className="text-[#B4B0AE]">
+                    Great if you're just getting started
+                </CardDescription>
+                <Link href="#" className="w-full mt-3">
+                    <Button
+                        variant="default"
+                        className="bg-[#333337] w-full rounded-2xl text-white hover:text-[#333337]"
+                    >
+                        Start for free
+                    </Button>
+                </Link>
+            </div>
+            <div className="flex flex-col gap-2 text-[#B4B0AE] mt-5">
+                <p>Features</p>
+                <span className="flex gap-2 mt-3 items-center">
+                    <Check />1 Group
+                </span>
+                <span className="flex gap-2 items-center">
+                    <Check />
+                    Anytime Access
+                </span>
+                <span className="flex gap-2 items-center">
+                    <Check />
+                    Unlimited Channels
+                </span>
+                <span className="flex gap-2 items-center">
+                    <Check />
+                    Payment option
+                </span>
+                <span className="flex gap-2 items-center">
+                    <Check />
+                    24/7 Support
+                </span>
+                <span className="flex gap-2 items-center">
+                    <Check />
+                    Custom Domain
+                </span>
+            </div>
+        </Card>
     </div>
   )
 }
