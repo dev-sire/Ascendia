5826150c1404cc1a86c6a3927b62c5586e7a1a67
dev-sire <shahidamaan2019@gmail.com>
2026-09-13 00:59:28 +0500

    Cloud Image fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactProductionProfiling: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "ucarecdn.com",
-            },
-        ],
-    },
+  reactProductionProfiling: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "k178x3aqyh.ucarecd.net",
+      },
+    ],
+  },
 }
 
 export default nextConfig
